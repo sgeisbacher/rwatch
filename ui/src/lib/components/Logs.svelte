d2c7dd5a642e1ef4609cb058f836a37fba20e244
Stefan Geisbacher <16902398+sgeisbacher@users.noreply.github.com>
2025-02-06 09:56:27 +0100

    polished ui to dark-mode (#10)

--- ui/src/lib/components/Logs.svelte
+++ ui/src/lib/components/Logs.svelte
@@ -1,17 +1,54 @@
 <script lang="ts">
 	import type { LogEvent } from '$lib/core/webrtc-manager.svelte';
 
+	let showLogs = $state(false);
+
 	interface Props {
 		logs: LogEvent[];
+	}
+
+	function toggleShowLogs() {
+		showLogs = !showLogs;
 	}
 	const { logs }: Props = $props();
 </script>
 
-<div>
-	Logs ({logs.length}):<br />
-	<ul>
-		{#each logs as log}
-			<li>{log.level[0].toUpperCase()} - {log.msg}</li>
-		{/each}
-	</ul>
+<div
+	id="logsContainer"
+	onclick={toggleShowLogs}
+	onkeyup={toggleShowLogs}
+	role="button"
+	tabindex="0"
+>
+	Logs ({logs.length})
+	{#if !showLogs}
+		-
+	{/if}
+	<span class="defensiveText">
+		{#if !showLogs}
+			{logs.length > 0 ? logs[logs.length - 1].msg : ''}
+		{/if}
+	</span><br />
+	{#if showLogs}
+		<ul>
+			{#each logs as log}
+				<li>
+					<span class="defensiveText">{log.timestamp.toLocaleTimeString()}</span>
+					{log.level.toUpperCase()} - {log.msg}
+				</li>
+			{/each}
+		</ul>
+	{/if}
 </div>
+
+<style>
+	#logsContainer {
+		margin-top: 30px;
+	}
+	.defensiveText {
+		color: gray;
+	}
+	ul {
+		list-style-type: none;
+	}
+</style>
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -16,5 +16,31 @@
 	});
 </script>
 
-<Terminal {data} />
-<Logs {logs} />
+<header>
+	<!-- Fixed navbar -->
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+		<a class="navbar-brand" href="#">rwatch</a>
+	</nav>
+</header>
+<!-- Begin page content -->
+<main role="main" class="container">
+	<div class="cover-container d-flex h-100 p-3 flex-column">
+		<Terminal {data} />
+		<Logs {logs} />
+	</div>
+</main>
+
+<footer class="footer">
+	<div class="container">
+		<span class="text-muted">Connection ...</span>
+	</div>
+</footer>
+
+<style>
+	.bg-dark {
+		background-color: #1e1e1e !important;
+	}
+	.footer {
+		background-color: #1e1e1e !important;
+	}
+</style>
